6448ed7dd317dedba4b479cc4038513558fa9250
Vaidehi Patel <vaidehi.patel@infarm.com>
2020-12-11 18:57:44 +0100

    dummy angular app

---
+++ src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { APP_BASE_HREF } from "@angular/common";
+
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [{ provide: APP_BASE_HREF, useValue: "/angular" }],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
